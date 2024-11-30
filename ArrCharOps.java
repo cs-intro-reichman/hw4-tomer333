4a465b506480adcf8b3f1b3a8a9935e5efe52ff2
tomer333 <tomer.dahan71@gmail.com>
2024-11-30 13:58:44 +0200

    Second Check

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -133,7 +133,7 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null) {return -2;}
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0) {return -2;}
         if (str1.equals(str2)) {return 0;}
 		for (int i = 0; i < Math.min(str1.length(), str2.length()); i++) {
 			if (str1.charAt(i) > str2.charAt(i)) {return 1;}
--- Primes.java
+++ Primes.java
@@ -1,6 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         int upTo =  Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to "+upTo);
         boolean[] arr = new boolean[upTo+1];
         for (int i = 2; i <= upTo; i++) {arr[i] = true;}
         int c = 2;
